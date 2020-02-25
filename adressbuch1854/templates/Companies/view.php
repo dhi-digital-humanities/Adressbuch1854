0c4b3e2dbe26cbf71b4bf99427b8a9f3aa3902b5
AlinaOs <47086948+AlinaOs@users.noreply.github.com>
2020-02-25 16:59:57 +0100

    Better code thanks to elements

--- adressbuch1854/templates/Companies/view.php
+++ adressbuch1854/templates/Companies/view.php
@@ -47,46 +47,7 @@
 					<th><?=__('Adresse(n)')?></th>
 					<td>
 					<?php if (!empty($company->addresses)) : ?>
-						<ul>
-							<?php foreach($company->addresses as $address): ?>
-							<?php
-								$streetOld = h($address->street->name_old_clean);
-								$streetNew = h($address->street->name_new);
-								$street;
-								if($streetOld === $streetNew){
-									$street = $streetOld;
-								} else {
-									$street = $streetOld.' ('.$streetNew.')';
-								}
-								
-								$housNo = h($address->houseno);
-								if(!empty($address->houseno_specification)){
-									$housNo.=' '.h($address->houseno_specification);
-								}
-								
-								$spec = h($address->address_specification_verbatim);
-													
-								$lat = $address->geo_lat;
-								$long = $address->geo_long;
-							?>
-							<li>
-								<?php
-									echo $this->Html->link($street, ['controller' => 'Streets', 'action' => 'view', $address->street->id]);
-									
-									echo ' '.$housNo;
-									
-									if(!empty($spec)){
-										echo ', '.$spec;
-									}
-									
-									/* Sollen die Geokoordinaten hier gezeigt werden (wenn vorhanden)? Dann diesen Code entkommentieren.
-									if(!empty($long) && !empty($lat )){
-										echo '<br>'.__('Geokoordinaten').': '.$lat.', '.$long.' '.__('(lat/long)');
-									}*/
-								?>
-							</li>
-							<?php endforeach; ?>
-						</ul>
+					<?= $this->element('addressList', ['addresses' => $company->addresses, 'list' => true]) ?>
 					<?php endif; ?>
 					</td>
 				</tr>
@@ -110,96 +71,8 @@
             </table>
             <?php if (!empty($company->persons)) : ?>
 			<div class="related">
-                <h4><?= __('Assoziierte Personen') ?></h4>
-                <div class="table-responsive">
-                    <table>
-                        <tr>
-							<th><?= __('Name') ?></th>
-							<th><?= __('Anmerkungen') ?></th>
-							<th><?= __('Beruf') ?></th>
-							<th><?= __('Adresse(n)') ?></th>
-							<th><?= __('Sonstige Merkmale') ?></th>
-							<th><?= __('Kategorien') ?></th>
-						</tr>
-                        <?php foreach ($company->persons as $person) : ?>
-                        <?php
-							$name = '';
-							if(!empty($person->title)){
-								$name.=h($person->title).' ';
-							}
-							if(!empty($person->name_predicate)){
-								$name.=h($person->name_predicate).' ';
-							}
-							$name.=h($person->surname);
-							if(!empty($person->first_name)){
-								$name.=', '.h($person->first_name);
-							}
-							
-							$cats = [];
-							if($person->has('prof_category')){
-								array_push($cats, $person->prof_category->name);
-							}
-							if($person->has('social_status') && $person->social_status->status != 'Commoner'){
-								array_push($cats, $person->social_status->status);
-							}
-							if($person->has('occupation_status') && $person->occupation_status->status != 'Active'){
-								array_push($cats, $person->occupation_status->status);
-							}
-							if($person->has('military_status') && $person->military_status->status != 'Civil'){
-								array_push($cats, $person->military_status->status);
-							}
-							
-							$plus = [];
-							if($person->has('ldh_rank')){
-								array_push($plus, $person->ldh_rank->rank);
-							}
-							if($person->de_l_institut){
-								array_push($plus, '(de l\'Institut)');
-							}
-							if($person->bold){
-								array_push($plus, __('Vorab-Abonnent'));
-							}
-							if($person->notable_commercant){
-								array_push($plus, 'Notable Commerçant');
-							}
-							if($person->advert){
-								array_push($plus, __('mit Geschäftseintrag'));
-							}
-						?>
-						<tr>
-							<td><?= $this->Html->link($name, ['controller' => 'Persons', 'action' => 'view', $person->id]) ?></td>
-							<td><?= h($person->specification_verbatim) ?></td>
-							<td><?= h($person->profession_verbatim) ?></td>
-							<td><?php
-								if (!empty($person->addresses)){
-									foreach ($person->addresses as $address){
-										$streetOld = h($address->street->name_old_clean);
-										$streetNew = h($address->street->name_new);
-										$street;
-										if($streetOld === $streetNew){
-											$street = $streetOld;
-										} else {
-											$street = $streetOld.' ('.$streetNew.')';
-										}
-										$housNo = h($address->houseno);
-										if(!empty($address->houseno_specification)){
-											$housNo.=' '.h($address->houseno_specification);
-										}
-										
-										echo $this->Html->link($street, ['controller' => 'Streets', 'action' => 'view', $address->street->id]);
-										
-										echo ' '.$housNo;
-										
-										echo '<br>';
-									}
-								}
-							?></td>
-							<td><?= implode(', ', $plus)?></td>
-							<td><?= implode(', ', $cats)?></td>
-						</tr>
-                        <?php endforeach; ?>
-                    </table>
-                </div>
+                <h4><?= __('Assoziierte Personen') ?></h4>							
+				<?= $this->element('personsMultiTable', ['persons' => $company->persons])?>
             </div>
             <?php endif; ?>
 			<?php if (!empty($company->external_references)) : ?>
---
+++ adressbuch1854/templates/element/personsMultiTable.php
@@ -0,0 +1,91 @@
+<?php
+/*
+Creates a table containing the main information about multiple companies. It uses the given array or query object with the variable name $persons.
+Adds a numbering if 'count' is set to true.
+Uses a list of addresses (instead of simple breaks) if addrAsList is set to true;
+*/
+
+if(!isset($count)){
+	$count=false;
+}
+
+if(!isset($addrAsList)){
+	$addrAsList=false;
+}
+?>
+<div class="table-responsive">
+	<table>
+		<tr>
+			<?php if($count):?>
+			<th><?= __('Nr') ?></th>
+			<?php endif;?>
+			<th><?= __('Name') ?></th>
+			<th><?= __('Anmerkungen') ?></th>
+			<th><?= __('Beruf') ?></th>
+			<th><?= __('Adresse(n)') ?></th>
+			<th><?= __('Sonstige Merkmale') ?></th>
+			<th><?= __('Kategorien') ?></th>
+		</tr>
+		<?php foreach ($persons as $person) : ?>
+		<?php
+			$name = '';
+			if(!empty($person->title)){
+				$name.=h($person->title).' ';
+			}
+			if(!empty($person->name_predicate)){
+				$name.=h($person->name_predicate).' ';
+			}
+			$name.=h($person->surname);
+			if(!empty($person->first_name)){
+				$name.=', '.h($person->first_name);
+			}
+			
+			$cats = [];
+			if($person->has('prof_category')){
+				array_push($cats, $person->prof_category->name);
+			}
+			if($person->has('social_status') && $person->social_status->status != 'Commoner'){
+				array_push($cats, $person->social_status->status);
+			}
+			if($person->has('occupation_status') && $person->occupation_status->status != 'Active'){
+				array_push($cats, $person->occupation_status->status);
+			}
+			if($person->has('military_status') && $person->military_status->status != 'Civil'){
+				array_push($cats, $person->military_status->status);
+			}
+			
+			$plus = [];
+			if($person->has('ldh_rank')){
+				array_push($plus, $person->ldh_rank->rank);
+			}
+			if($person->de_l_institut){
+				array_push($plus, '(de l\'Institut)');
+			}
+			if($person->bold){
+				array_push($plus, __('Vorab-Abonnent'));
+			}
+			if($person->notable_commercant){
+				array_push($plus, 'Notable Commerçant');
+			}
+			if($person->advert){
+				array_push($plus, __('mit Geschäftseintrag'));
+			}
+		?>
+		<tr>
+			<?php if($count):?>
+			<td><?= $this->Number->format($countNo)?></td>
+			<?php endif;?>
+			<td><?= $this->Html->link($name, ['controller' => 'Persons', 'action' => 'view', $person->id]) ?></td>
+			<td><?= h($person->specification_verbatim) ?></td>
+			<td><?= h($person->profession_verbatim) ?></td>
+			<td><?php
+				if (!empty($person->addresses)){
+					echo $this->element('addressList', ['addresses' => $person->addresses, 'list' => $addrAsList]);
+				}
+			?></td>
+			<td><?= implode(', ', $plus)?></td>
+			<td><?= implode(', ', $cats)?></td>
+		</tr>
+		<?php endforeach; ?>
+	</table>
+</div>
