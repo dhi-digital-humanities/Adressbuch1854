cec6b10fc667a86348cd335e91f80ef97ea4276f
Evan Virevialle <83653214+evirevialle@users.noreply.github.com>
2021-10-15 10:45:32 +0200

    Add files via upload

--- templates/Companies/view.php
+++ templates/Companies/view.php
@@ -36,12 +36,12 @@ require(__DIR__.'/../functions/varscomp.php');
             	<tr>
             		<th><?= __('Scan der Seite')?></th>
             		<td>
-							<?php print image('/../scans/','SD/','BHVP_703983_',$begP);?><br>
+							<?php print image('http://adressbuch1854.dh.uni-koeln.de/scans/','SD/','BHVP_703983_',$begP);?><br>
 						
 						<details>
 							<summary><?= __('Seite in HD ansehen')?></summary>
 						<form>
-							<button type='submit' title="IHA zur Nutzung der Seite <?php echo $begP?>" formtarget='_blank' formaction='/../scans/HD/BHVP_703983_<?php echo $begP ?>.jpg'
+							<button type='submit' title="IHA zur Nutzung der Seite <?php echo $begP?>" formtarget='_blank' formaction='http://adressbuch1854.dh.uni-koeln.de/scans/HD/BHVP_703983_<?php echo $begP ?>.jpg'
 
 							value="text">BHVP_703983_<?php echo $begP?>.jpg</button>
 						</form>
