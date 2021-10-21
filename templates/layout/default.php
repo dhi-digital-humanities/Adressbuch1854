3e4e976eb845893ed793a9b19e4187f6d1bcf75f
Evan Virevialle <83653214+evirevialle@users.noreply.github.com>
2021-10-21 11:45:21 +0200

    Add files via upload

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -40,7 +40,6 @@ $description = 'Adressbuch 1854';
     <?= $this->Html->css('style.css') ?>
 
 	<!-- Leaflet css -->
-
 	<?= $this->Html->css('https://unpkg.com/leaflet@1.6.0/dist/leaflet.css', ['integrity' => 'sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==', 'crossorigin' => '']) ?>
     <?= $this->Html->css('https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.5.0/MarkerCluster.Default.css') ?>
     <?= $this->Html->css('https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.5.0/MarkerCluster.css') ?>
@@ -51,15 +50,6 @@ $description = 'Adressbuch 1854';
     <?= $this->Html->css('leaflet-sidebar.css') ?>
     <?= $this->Html->css('https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css') ?>
 
-	<?= $this->Html->css('https://unpkg.com/leaflet@1.6.0/dist/leaflet.css', ['integrity' => 'sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==', 'crossorigin' => '']);?>
-    <?= $this->Html->css('https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.5.0/MarkerCluster.Default.css'); ?>
-    <?= $this->Html->css('https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.5.0/MarkerCluster.css'); ?>
-    <?= $this->Html->css('https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.5.0/MarkerCluster.Default.min.css'); ?>
-    <?= $this->Html->css('https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css'); ?>
-    <?= $this->Html->css('Control.Opacity.css'); ?>
-    <?= $this->Html->css('jquery-ui-1.10.3.custom.min.css'); ?>
-
-
 
 	<!-- JQuery script -->
 	<?= $this->Html->script('https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js') ?>
@@ -67,7 +57,6 @@ $description = 'Adressbuch 1854';
 	<!-- Leaflet script -->
 	<?= $this->Html->script('https://unpkg.com/leaflet@1.6.0/dist/leaflet.js', ['integrity' => 'sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==', 'crossorigin' => '']) ?>
    
-
     <?= $this->Html->script('https://cdnjs.cloudflare.com/ajax/libs/OverlappingMarkerSpiderfier-Leaflet/0.2.6/oms.min.js') ?>
     <?= $this->Html->script('https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.5.0/leaflet.markercluster-src.js') ?>
    <?= $this->Html->script('https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js') ?>
@@ -77,14 +66,6 @@ $description = 'Adressbuch 1854';
    <?= $this->Html->script('leaflet-sidebar.js') ?>
    <?= $this->Html->script('leaflet.shpfile.js') ?>
    <?= $this->Html->script('shp.js') ?>
-
-    <?= $this->Html->script('https://cdnjs.cloudflare.com/ajax/libs/OverlappingMarkerSpiderfier-Leaflet/0.2.6/oms.min.js'); ?>
-    <?= $this->Html->script('https://cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/1.5.0/leaflet.markercluster-src.js'); ?>
-   <?= $this->Html->script('https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js'); ?>
-   <?= $this->Html->script('Control.Opacity.js'); ?>
-   <?= $this->Html->script('jquery-1.9.1.js'); ?>
-   <?= $this->Html->script('jquery-ui-1.10.3.custom.min.js'); ?> 
-
 
 	<!-- fetched meta -->
 	<?= $this->fetch('meta') ?>
@@ -126,7 +107,7 @@ $description = 'Adressbuch 1854';
     </main>
     <footer>
 		<a target="_blank" href="https://www.dhi-paris.fr/">
-			<?=$this->Html->image('logo.png', ['class' => 'wrapper-item', 'id' => 'dhi-logo', 'alt' => 'Logo DHIP', 'width' => '100px'])?>
+			<?=$this->Html->image('/../scans/logo.png', ['class' => 'wrapper-item', 'id' => 'dhi-logo', 'alt' => 'Logo DHIP', 'width' => '100px'])?>
 		</a>
 		<?= $this->Html->link(__('Impressum'), ['controller' => 'Pages', 'action' => 'credits'], ['class' => 'wrapper-item', 'id' => 'credits']) ?>
     </footer>
