a54080b25defc79d56b731abc6b170c47afb3ca4
Evan Virevialle <83653214+evirevialle@users.noreply.github.com>
2022-04-21 09:50:35 +0200

    Update personsMultiTable.php

--- templates/element/personsMultiTable.php
+++ templates/element/personsMultiTable.php
@@ -84,7 +84,7 @@ $this->Html->css('multiTable.css');
 			<?php if($count):?>
 			<td><?= $this->Number->format($countNo)?></td>
 			<?php endif;?>
-			<td><?= $this->Html->link(htmlspecialchars_decode($name), ['controller' => 'Persons', 'action' => 'view', $person->id]) ?></td>
+			<td><?= $this->Html->link(htmlspecialchars_decode($name, ENT_QUOTES), ['controller' => 'Persons', 'action' => 'view', $person->id]) ?></td>
 			
 			<td><?= h($person->profession_verbatim) ?></td>
 			<td><?php
